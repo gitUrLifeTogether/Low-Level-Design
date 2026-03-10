847b64d87a4a5f3cb1bc4290c7905d3fb90e0fd3
gitUrLifeTogether <sandhaletech@gmail.com>
2026-03-10 09:58:39 +0530

    SRP violation Example

---
+++ 01_SOLID-Principles/01_A_SRP_violation.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Product{
+    public:
+     double price;
+     string name;
+
+     Product(string name,double price){
+        this->name=name;
+        this->price=price;
+     }
+};
+
+class ShoppingCart{
+    private:
+    vector<Product*> products;
+
+    public:
+     void addProduct(Product* p){
+        products.push_back(p);
+     }
+
+    const vector<Product*>&getProducts(){
+        return products;
+    }
+
+    double calculateTotal(){
+        double total =0;
+        for(auto p:products){
+            total+=p->price;
+        }
+        return total;
+    }
+
+    void printInvoice(){
+        cout<<"Shopping Cart Invoice:\n";
+        for(auto p:products){
+            cout<<p->name<<" $" <<p->price<<endl;
+        }
+        cout<<"Total: $"<<calculateTotal()<<endl;
+    }
+
+    void saveToDataBase(){
+        cout<<"saving shopping cart to database...."<<endl;
+    }
+};
+ 
+int main(){
+    ShoppingCart* cart= new ShoppingCart;
+
+    cart->addProduct(new Product("Laptop",1500));
+    cart->addProduct(new Product("Mouse",50));
+
+    cart->printInvoice();
+    cart->saveToDataBase();
+
+    return 0;
+   
+
+   }
