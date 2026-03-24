152762fb7f9a642f8c18490acb6b394958b6eea6
gitUrLifeTogether <sandhaletech@gmail.com>
2026-03-24 23:35:07 +0530

    SRP violation example

--- 01_SOLID-Principles/01_SRP/01_A_SRP_violation.cpp
+++ 01_SOLID-Principles/01_SRP/01_A_SRP_violation.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 // this is an example thaat violates single responsibility principle
+
+
 class Product{
     public:
      double price;
@@ -61,5 +63,3 @@ int main(){
 
    }
 
-
-   
